7293a782fc6c88159be4f4ba43b817d09eb09353
LubiTomi <124986213+LubiTomi@users.noreply.github.com>
2023-06-22 14:52:13 +0900

    first commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import pi from './pikachu1.gif';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={pi} width={300} border="1"/>
+      <br/><br/>
+      <h2>Docker 배포 성공 기원 !!!</h2>
+    </div>
+  );
+}
+
+export default App;
